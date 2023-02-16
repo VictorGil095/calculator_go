648790c368796c1d65817d0bcd7ad09ff9b923b7
VictorGil095 <victorgil444@gmail.com>
2023-02-16 12:27:58 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/VictorGil095/calculator-go/lib"
+	"os"
+)
+
+func formatFloat(num float64) string {
+	str := fmt.Sprintf("%.9f", num)
+	truncate := len(str)
+
+	for i := len(str) - 1; i >= 0; i-- {
+		if str[i] == '0' {
+			truncate = i
+		}
+	}
+
+	if truncate > 0 && str[truncate-1] == '.' {
+		truncate--
+	}
+
+	return str[0:truncate]
+}
+
+func main() {
+	for true {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		input, _ := reader.ReadString('\n')
+		lexer := lib.Lexer(input)
+		interpreter := lib.Interpreter(&lexer)
+		result, err := interpreter.Result()
+
+		if err == nil {
+			fmt.Printf("%s\n", formatFloat(result))
+		} else {
+			fmt.Printf("%s\n", err)
+		}
+	}
+}
