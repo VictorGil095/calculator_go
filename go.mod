474833979d0b64a16f29a52fd006f951381ebb84
VictorGil095 <victorgil444@gmail.com>
2023-03-12 23:40:22 +0300

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VictorGil095/calculator_go/tree/main/lib
+
+go 1.20
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/VictorGil095/calculator-go/lib"
 	"os"
+
+	"github.com/VictorGil095/calculator_go/tree/main/lib"
 )
 
 func formatFloat(num float64) string {
